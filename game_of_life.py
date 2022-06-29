60a29413f6dcec52f69b90dcbd67854ea81c72f2
m254788 <m254788@usna.edu>
2022-06-29 10:05:28 -0500

    new update

--- game_of_life.py
+++ game_of_life.py
@@ -1,7 +1,5 @@
 import numpy as np
-
-n = 10
-board = np.zeros((n,n))
+import random
 
 def sum_neighbors(board,r,c):
 	neighbors = []
@@ -11,10 +9,11 @@ def sum_neighbors(board,r,c):
 				neighbors.append(board[i,j])
 	return sum(neighbors)
 
-def tick(board_in, n):
+def tick(board_in):
 	board_out = np.zeros(board_in.shape)
-	for i in range(1,n-1):
-		for j in range(1,n-2):
+	n = board_in.shape
+	for i in range(1,board_in.shape[0]-1):
+		for j in range(1,board_in.shape[1]-1):
 			if board_in[i][j]:
 				if sum_neighbors(board_in, i, j) in [2,3]:
 					board_out[i][j] = 1
@@ -27,9 +26,17 @@ def tick(board_in, n):
 					board_out[i][j] = 0
 	return board_out
 
-
-
-
+def play(rounds, board_size):
+	board = np.zeros((board_size, board_size))
+	for i in range(board_size):
+		for j in range(board_size):
+			board[i,j] = random.randint(0,1)
+	
+	for r in range(rounds):
+		print (board)
+		board = tick(board)
+ 
+play(10, 10)
 				
 			
 
